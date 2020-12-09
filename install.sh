c9fb9200fdfe7e85d4b35ad77f0b30733b353bcc
webmasterepage <72214792+webmasterepage@users.noreply.github.com>
2020-12-10 01:58:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -e ngrok.service ]; then
-    git clone --depth=1 https://github.com/vincenthsu/systemd-ngrok.git
+    git clone --depth=1 https://github.com/webmasterepage/systemd-ngrok.git
     cd systemd-ngrok
 fi
 cp ngrok.service /lib/systemd/system/
